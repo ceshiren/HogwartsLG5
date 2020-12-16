48c08bc5f92d8c8051b6a7dfe592390ee55b8a63
yan <294145891@qq.com>
2020-12-16 13:27:23 +0800

    :author : yan 作业操作，代码更新

--- test_git/git_demo.py
+++ test_git/git_demo.py
@@ -1,1 +1,2 @@
 print('seveniruby')
+print("lg5_yan")
--- test_python/src/demo.py
+++ test_python/src/demo.py
@@ -1,3 +1,4 @@
 print("hello from hogwarts@ceshiren.com")
 print('seveniruby')
 print('hogwarts')
+print("yan https://github.com/GDFuturexx/HogwartsLG5")
