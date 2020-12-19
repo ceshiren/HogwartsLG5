cb321436b83ad6915dcebc50902ebe24d5310d53
kisslife_98 <582238977@qq.com>
2020-12-19 22:06:27 +0800

    '九九乘法表'

--- test_git/git_demo.py
+++ test_git/git_demo.py
@@ -1,2 +1,9 @@
 print('seveniruby')
 print('Hello,Hogwarts! My name is wangyuhang!')
+#九九乘法表
+for i in range(1,10):
+    for j in range(1,i+1):
+        print(f"{j}*{i}={i*j}",end='  ')
+    print()
+
+
