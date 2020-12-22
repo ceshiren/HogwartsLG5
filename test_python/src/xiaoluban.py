c29ca6857e4c2feda9b9e37d89bf4bc746f89235
李文良 <li.wenliang@foxmail.com>
2020-12-22 15:20:01 +0800

    李文良增加小鲁班信息！！！

---
+++ test_python/src/xiaoluban.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# @project : HogwartsLG5
+# @File    : xiaoluban.py
+# @Date    : 2020/12/22 14:56
+# @Author  : 李文良
+from test_python.src.hero import Hero
+
+
+class Xiaoluban(Hero):
+    hp = 80
+    speed = 2
+    name='小鲁班'
+    tools=[]
