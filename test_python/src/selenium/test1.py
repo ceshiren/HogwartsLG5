0d0441b95396d412f01b178803f406595d3103f9
王刚 <15398100909@163.com>
2021-01-16 12:01:11 +0800

    web自动化

--- test_python/src/selenium/test1.py
+++ test_python/src/selenium/test1.py
@@ -1,4 +1,4 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome()
-driver.get("http://www.baidu.com")
+driver = webdriver.Chrome(r"D:\chrome\Google\Chrome\Application\chrome.exe")
+driver.get("https://www.baidu.com")
