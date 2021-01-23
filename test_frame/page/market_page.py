0fc3ba425384c6447d0f41f10842cf5b64295445
ruotongyu <yuruotong1@163.com>
2021-01-23 21:32:49 +0800

    测试框架第二节课

--- test_frame/page/market_page.py
+++ test_frame/page/market_page.py
@@ -1,3 +1,4 @@
+import yaml
 from selenium.webdriver.common.by import By
 
 from test_frame.basepage import BasePage
@@ -6,5 +7,6 @@ from test_frame.page.search_page import SearchPage
 
 class MarketPage(BasePage):
     def goto_search(self):
-        self.find_and_click((By.XPATH, "//*[@resource-id='com.xueqiu.android:id/action_search']"))
+        self.run_steps("../page/market_page.yaml", "goto_search")
+        # self.find_and_click((By.XPATH, "//*[@resource-id='com.xueqiu.android:id/action_search']"))
         return SearchPage(self.driver)
--- test_frame/testcase/test_search.py
+++ test_frame/testcase/test_search.py
@@ -5,3 +5,8 @@ def test_search():
     app = App()
     result = app.start().goto_main().goto_market_page().goto_search().search()
     assert result
+
+def test_mine():
+    app = App()
+    app.start().goto_main().goto_mine()
+
