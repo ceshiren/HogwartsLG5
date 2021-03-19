697fb4d80800e0fdc2e1317421c84b5cee999eb1
wanggang <gang.wang2@dmall.com>
2021-03-19 16:36:05 +0800

    backend

---
+++ test_git/testfor.py
@@ -0,0 +1,11 @@
+s=["珍珍","爱爱","莲莲"]
+print("八戒".join(["%s" % s1 for s1 in s]))
+
+for s1 in s:
+    print("八戒".join(["%s" % s1]))
+
+print("八戒".join(["珍珍","爱爱","莲莲"]))
+
+print("八戒".join(["珍珍"]))
+
+print(["%s" % s1 for s1 in s])
