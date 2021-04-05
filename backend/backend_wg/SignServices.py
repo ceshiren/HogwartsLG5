77a98432aca42487eafd758870255f233ed8eaf4
王刚 <15398100909@163.com>
2021-04-05 11:31:31 +0800

    platform1

--- backend/backend_wg/SignServices.py
+++ backend/backend_wg/SignServices.py
@@ -3,8 +3,10 @@ import json
 from flask import Flask, flash, request
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 app.config[
     'SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://hogworts:hogworts@47.110.37.80:49158/test_platform?charset=utf8mb4'
@@ -16,12 +18,12 @@ https://flask-sqlalchemy.palletsprojects.com/en/2.x/
 
 
 class TestUser(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=False, nullable=False)
-    steps = db.Column(db.String(1000), unique=False, nullable=True)
+    username = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(1000), unique=False, nullable=True)
 
     def __repr__(self):
-        return '<TestCase %r>' % self.name
+        return '<TestCase %r>' % self.username
 
 
 """
@@ -33,54 +35,54 @@ https://flask-restful.readthedocs.io/en/latest/quickstart.html#
 class SignUpServices(Resource):
     # testcases=[]
     def get(self):
-        name = request.args.get('name', None)
-        if name:
-            testcase = TestUser.query.filter_by(name=name).first()
+        username = request.args.get('username', None)
+        if username:
+            testcase = TestUser.query.filter_by(username=username).first()
             return {'code': "0000", 'message': str(testcase), 'success': True}
         else:
             testcases = TestUser.query.all()
             return {'code': "0000", 'message': [str(testcase) for testcase in testcases], 'success': True}
 
     def post(self):
-        name = request.json.get('name'),
+        username = request.json.get('username'),
 
         testcase = TestUser(
-            id=request.json.get("id"),
-            name=request.json.get('name'),
-            steps=json.dumps(request.json.get("steps"))
+            username=request.json.get("username"),
+            email=request.json.get('email'),
+            password=json.dumps(request.json.get("password"))
         )
 
-        query_testcase = TestUser.query.filter_by(name=name).first()
+        query_testcase = TestUser.query.filter_by(username=username).first()
         if query_testcase:
-            return {'code': "0001", 'message': f'{name} is existed', 'success': False}
+            return {'code': "0001", 'message': f'{username} is existed', 'success': False}
         else:
             db.session.add(testcase)
             db.session.commit()
-            testcase = TestUser.query.filter_by(name=name).first()
+            testcase = TestUser.query.filter_by(username=username).first()
             return {'code': "0000", 'message': str(testcase), 'success': True}
 
     def put(self):
-        name = request.args.get('name', None)
+        username = request.args.get('username', None)
         testcase = TestUser(
-            id=request.json.get("id"),
-            name=request.json.get('name'),
-            steps=json.dumps(request.json.get("steps"))
+            username=request.json.get("username"),
+            email=request.json.get('email'),
+            password=json.dumps(request.json.get("password"))
         )
-        if name:
+        if username:
             # name=name 中间不可以加空格
-            query_testcase = TestUser.query.filter_by(name=name).first()
+            query_testcase = TestUser.query.filter_by(username=username).first()
             if query_testcase:
-                return {'code': "0001", 'message': f'{name} is existed', 'success': False}
+                return {'code': "0001", 'message': f'{username} is existed', 'success': False}
             else:
                 db.session.add(testcase)
                 db.session.commit()
                 return {'code': "0000", 'message': 'completed', 'success': True}
         else:
-            return {'code': "0002", 'message': f'{name} is not found,please check', 'success': False}
+            return {'code': "0002", 'message': f'{username} is not found,please check', 'success': False}
 
     def delete(self):
-        name = request.json['name']
-        testcase = TestUser.query.filter_by(name=name).first()
+        name = request.json['username']
+        testcase = TestUser.query.filter_by(username=username).first()
         if testcase:
             db.session.delete(testcase)
             db.session.commit()
@@ -93,21 +95,21 @@ class SignInServices(Resource):
     # testcases=[]
     def post(self):
 
-        name = request.json.get('name'),
+        username = request.json.get('username'),
 
         testcase = TestUser(
-            id=request.json.get("id"),
-            name=request.json.get('name'),
-            steps=json.dumps(request.json.get("steps"))
+            username=request.json.get("username"),
+            email=request.json.get('email'),
+            password=json.dumps(request.json.get("password"))
         )
 
-        query_testcase = TestUser.query.filter_by(name=name).first()
+        query_testcase = TestUser.query.filter_by(username=username).first()
         if query_testcase:
-            return {'code': "0001", 'message': f'{name} is existed', 'success': False}
+            return {'code': "0001", 'message': f'{username} is existed', 'success': False}
         else:
             db.session.add(testcase)
             db.session.commit()
-            testcase = TestUser.query.filter_by(name=name).first()
+            testcase = TestUser.query.filter_by(username=username).first()
             return {'code': "0000", 'message': str(testcase), 'success': True}
 
 
